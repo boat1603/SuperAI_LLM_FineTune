72ac613d3905cbe404d5ea2489097b334b5e4e52
unknown <chanon.utupon@gmail.com>
2024-05-01 00:04:45 +0700

    fix_tokenize

--- src/llm_finetune/tokenize.py
+++ src/llm_finetune/tokenize.py
@@ -1,28 +1,20 @@
 import transformers
-from typing import Sequence, Dict
+from typing import Dict
 
 
-def _tokenize_fn(
-    strings: Sequence[str], tokenizer: transformers.PreTrainedTokenizer
-) -> Dict:
-    """Tokenize a list of strings."""
-    tokenized_list = [
-        tokenizer(
-            text,
-            return_tensors="pt",
-            padding="longest",
-            max_length=tokenizer.model_max_length,
-            truncation=True,
-        )
-        for text in strings
-    ]
-    input_ids = labels = [
-        tokenized.input_ids[0] for tokenized in tokenized_list
-    ]  # noqa: E501
-    input_ids_lens = labels_lens = [
-        tokenized.input_ids.ne(tokenizer.pad_token_id).sum().item()
-        for tokenized in tokenized_list
-    ]
+def _tokenize_fn(text: str, tokenizer: transformers.PreTrainedTokenizer) -> Dict:
+    """Tokenize a string."""
+    text_tokenized = tokenizer(
+        text,
+        return_tensors="pt",
+        padding="longest",
+        max_length=tokenizer.model_max_length,
+        truncation=True,
+    )
+    input_ids = labels = text_tokenized.input_ids[0]
+    input_ids_lens = labels_lens = (
+        text_tokenized.input_ids.ne(tokenizer.pad_token_id).sum().item()
+    )
     return dict(
         input_ids=input_ids,
         labels=labels,
